20ed3def06fdd5c29b3cd035efe7be852c154ba2
Mangeshkapgate <mangesh_kapgate@epam.com>
2026-04-10 10:19:32 +0200

    Added PyTest CSV validation tasks to the project

--- PyTest Introduction/tests/conftest.py
+++ PyTest Introduction/tests/conftest.py
@@ -1,10 +1,35 @@
 import pytest
-import pandas as pd
+import csv
+import os
 
 # Fixture to read the CSV file
+@pytest.fixture(scope="session")
+def csv_data():
+    base_dir = os.path.dirname(os.path.dirname(__file__))
+
+    file_path = os.path.join(base_dir, "src", "data", "data.csv")
+    with open(file_path, mode = 'r') as f:
+        reader = csv.DictReader(f)
+        data =  list(reader)
+        return data
+
+
+
+
+
 
 
 # Fixture to validate the schema of the file
+@pytest.fixture(scope = "session")
+def schema_validate():
+    def _validate(expected_schema, actual_schema):
+        return set(expected_schema) == set(actual_schema)
+    return _validate
 
 
 # Pytest hook to mark unmarked tests with a custom mark
+def pytest_collection_modifyitems(config, items):
+    for item in items:
+        if not item.own_markers:
+            item.add_marker("unmarked")
+
--- PyTest Introduction/tests/test_csv/test_csv_file.py
+++ PyTest Introduction/tests/test_csv/test_csv_file.py
@@ -1,30 +1,54 @@
 import pytest
-import re
+
+def test_file_not_empty(csv_data):
+    assert len(csv_data) > 0, "CSV file is empty"
+
+@pytest.mark.validate_csv
+@pytest.mark.xfail(reason = "known duplicates")
+def test_duplicates(csv_data):
+    rows = []
+
+    for row in csv_data:
+        rows.append(tuple(row.values()))
+
+    assert len(rows) == len(set(rows)), "Duplicate rows found"
 
 
-def test_file_not_empty():
-    assert 1 + 1 == 2
 
 
-def test_duplicates():
-    assert 1 + 1 == 2
+@pytest.mark.validate_csv
+def test_validate_schema(csv_data,schema_validate):
+    actual_schema = csv_data[0].keys()
+    expected_schema = ['id','name','age','email','is_active']
+    assert schema_validate(expected_schema,actual_schema), "Schema mismatch"
+
+@pytest.mark.validate_csv
+@pytest.mark.skip(reason = "skip age validation")
+def test_age_column_valid(csv_data):
+    for row in csv_data:
+        age = int(row["age"])
+        assert 0 <= age <= 100, f"Invalid age: {age}"
 
 
-def test_validate_schema():
-    assert 1 + 1 == 2
+
+@pytest.mark.validate_csv
+def test_email_column_valid(csv_data):
+    for row in csv_data:
+        email = row["email"]
+        assert "@" in email and "." in email and "com" in email, f"Invalid email: {email}"
 
 
-def test_age_column_valid():
-    assert 1 + 1 == 2
+@pytest.mark.parametrize("id,is_active",[("1","False"),("2","True")])
+def test_active_players_parameter(csv_data,id,is_active):
+    for row in csv_data:
+        if row["id"] == id:
+            assert row["is_active"] == is_active, f"id has wrong value: {id}"
 
 
-def test_email_column_valid():
-    assert 1 + 1 == 2
 
-
-def test_active_players():
-    assert 1 + 1 == 2
-
-
-def test_active_player():
-    assert 1 + 1 == 2
+def test_active_player(csv_data):
+    for row in csv_data:
+        id = row["id"]
+        is_active = row["is_active"]
+        if id == "2":
+            assert is_active == "True", "should be True"
